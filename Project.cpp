feafdd1c6227d552f64348a588c463ad2cc4f28b
MisterrHappy <andremarinho_1@hotmail.com>
2020-03-07 16:18:34 +0000

    Beginning

---
+++ Project.cpp
@@ -0,0 +1,19 @@
+/* First ASA project               */
+/* Authors: Andre Marinho 93687    */
+/*          Joao Domingos 92726    */
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+
+    if (argc != 1) {
+        cout << "Invalid number of arguments\n";
+        exit(EXIT_FAILURE);
+    }
+
+    
+
+    exit(EXIT_SUCCESS);
+}
