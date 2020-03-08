541c1486af399adabc82200146d87870033fb30e
MisterrHappy <andremarinho_1@hotmail.com>
2020-03-08 20:02:22 +0000

    ...

--- Project.cpp
+++ Project.cpp
@@ -5,44 +5,33 @@
 #include <iostream>
 #include <vector>
 #include <stack>
+#include <algorithm>
 
 using namespace std;
-
-/*class Vertex {
-
-    private:
-        int grade;
-
-    public:
-        Vertex(int grade) {
-            this->grade = grade;
-        }
-
-        void changeGrade(int g) {
-            grade = g;
-        }
-}; */
 
 class DirectedGraph {
 
     private:
         int vNumber;
         vector<int>* adj;
+        vector<int>* SCCs;
 
     public:
-        DirectedGraph(int vNumber) {
-            this->vNumber = vNumber;
+        DirectedGraph(int vNumber) 
+        : vNumber(vNumber), adj(new vector<int>[vNumber]), SCCs(new vector<int>[vNumber]) {}
 
-            adj = new vector<int>[vNumber];
+        ~DirectedGraph() {
+            delete[]adj;
+            delete[]SCCs;
         }
 
         void addEdge(int vertex, int edge) {
             adj[vertex].push_back(edge);
         }
 
-        void SCCUtil(int u, int disc[], int low[], stack<int> *st, bool stackMember[], int* grades, int* SCCs) { 
+        void SCCSubBuild(int u, int disc[], int low[], stack<int> *st, bool stackMember[], int* grades, int SCCPos) { 
         
-            int time = 0, w = 0, x = 0, maxGrade = 0, y;
+            int time = 0, w = 0, x = 0, maxGrade = 0;
 
             disc[u] = low[u] = ++time; 
             st->push(u); 
@@ -53,57 +42,66 @@ class DirectedGraph {
                 int v = *i;   
             
                 if (disc[v] == -1) { 
-                    SCCUtil(v, disc, low, st, stackMember, grades, SCCs); 
+                    SCCSubBuild(v, disc, low, st, stackMember, grades, SCCPos);
+                    maxGrade = 0; 
                     low[u]  = min(low[u], low[v]); 
                 } 
     
                 else if (stackMember[v] == true) 
-                    low[u]  = min(low[u], disc[v]); 
-            }   
+                    low[u] = min(low[u], disc[v]); 
+            }
+
             if (low[u] == disc[u]) {
                 while (st->top() != u) { 
                     w = (int) st->top(); 
-                    SCCs[x++] = w;
+
                     stackMember[w] = false;
+                    SCCs[SCCPos].push_back(w);
+
+                    if (grades[w - 1] > maxGrade)
+                        maxGrade = grades[w - 1];
+
                     st->pop(); 
                 } 
                 w = (int) st->top(); 
                
                 stackMember[w] = false;
-                SCCs[x++] = w;
 
-                if (grades[w -1] > maxGrade)
+                SCCs[SCCPos].push_back(w);
+
+                if (grades[w - 1] > maxGrade)
                     maxGrade = grades[w - 1];
 
                 st->pop(); 
             }
-            // for (y = 0; )
-
-           
-            // while (y-- != -1)
-            //     SCCs[y] = -1;
-
+            vector<int>::iterator j;
+            for (j = SCCs[SCCPos].begin(); j != SCCs[SCCPos].end(); ++j) {
+                int g = *j;
+                grades[g - 1] = maxGrade;
+            }
+            ++SCCPos;
         }
 
-        void SCC(int* grades) { 
+        void SCCBuild(int* grades) { 
+            int SCCPos = 0;
             int *disc = new int[vNumber]; 
             int *low = new int[vNumber]; 
             bool *stackMember = new bool[vNumber]; 
             stack<int> *st = new stack<int>(); 
-            int *SCCs = new int(vNumber);
     
             for (int x = 0; x < vNumber; x++) { 
                 disc[x] = -1; 
                 low[x] = -1; 
                 stackMember[x] = false; 
-                SCCs[x] = -1;
             }
 
             for (int x = 0; x < vNumber; x++) 
                 if (disc[x] == -1) 
-                    SCCUtil(x, disc, low, st, stackMember, grades, SCCs); 
-            
-            //delete?
+                    SCCSubBuild(x, disc, low, st, stackMember, grades, SCCPos);
+            delete[]disc;
+            delete[]low;
+            delete[]stackMember;
+            delete[]st; 
         } 
 
 };
@@ -111,12 +109,14 @@ class DirectedGraph {
 int main(int argc, char* argv[]) {
 
     int vertexes, edges, mark, *grades, s1, s2, x = 0;
+    int graphSize;
 
     scanf("%d,%d", &vertexes, &edges);
+    graphSize = vertexes;
 
     DirectedGraph graph(vertexes);
 
-    grades = new int[vertexes]; //fazer delete
+    grades = new int[vertexes];
 
     while (vertexes-- != 0) {
         scanf("%d", &mark);
@@ -128,7 +128,12 @@ int main(int argc, char* argv[]) {
         graph.addEdge(s1, s2);
     }
 
-    delete(grades); // loop?
+    graph.SCCBuild(grades);
+
+    for (x = 0; x < graphSize; x++)
+        cout << grades[x] << "\n";
+
+    delete[]grades;
 
     exit(EXIT_SUCCESS);
 }
