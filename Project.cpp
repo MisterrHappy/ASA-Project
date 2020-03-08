62bcf852ff19ffe4b28b64daac5a78c3650776a2
MisterrHappy <andremarinho_1@hotmail.com>
2020-03-08 18:20:05 +0000

    Tem q se ver

--- Project.cpp
+++ Project.cpp
@@ -40,10 +40,10 @@ class DirectedGraph {
             adj[vertex].push_back(edge);
         }
 
-        void SCCUtil(int u, int disc[], int low[], stack<int> *st, bool stackMember[]) { 
+        void SCCUtil(int u, int disc[], int low[], stack<int> *st, bool stackMember[], int* grades, int* SCCs) { 
         
-            int time = 0; 
-    
+            int time = 0, w = 0, x = 0, maxGrade = 0, y;
+
             disc[u] = low[u] = ++time; 
             st->push(u); 
             stackMember[u] = true; 
@@ -53,43 +53,57 @@ class DirectedGraph {
                 int v = *i;   
             
                 if (disc[v] == -1) { 
-                    SCCUtil(v, disc, low, st, stackMember); 
-
+                    SCCUtil(v, disc, low, st, stackMember, grades, SCCs); 
                     low[u]  = min(low[u], low[v]); 
                 } 
     
                 else if (stackMember[v] == true) 
                     low[u]  = min(low[u], disc[v]); 
-            } 
-            int w = 0;   
-            if (low[u] == disc[u]) { 
+            }   
+            if (low[u] == disc[u]) {
                 while (st->top() != u) { 
                     w = (int) st->top(); 
-                    cout << w << " "; 
-                    stackMember[w] = false; 
+                    SCCs[x++] = w;
+                    stackMember[w] = false;
                     st->pop(); 
                 } 
                 w = (int) st->top(); 
-                cout << w << "\n"; 
-                stackMember[w] = false; 
+               
+                stackMember[w] = false;
+                SCCs[x++] = w;
+
+                if (grades[w -1] > maxGrade)
+                    maxGrade = grades[w - 1];
+
                 st->pop(); 
-            } 
+            }
+            // for (y = 0; )
+
+           
+            // while (y-- != -1)
+            //     SCCs[y] = -1;
+
         }
 
-        void SCC() { 
+        void SCC(int* grades) { 
             int *disc = new int[vNumber]; 
             int *low = new int[vNumber]; 
             bool *stackMember = new bool[vNumber]; 
             stack<int> *st = new stack<int>(); 
+            int *SCCs = new int(vNumber);
     
-            for (int i = 0; i < vNumber; i++) { 
-            disc[i] = -1; 
-            low[i] = -1; 
-            stackMember[i] = false; 
-        } 
-            for (int i = 0; i < vNumber; i++) 
-                if (disc[i] == -1) 
-                    SCCUtil(i, disc, low, st, stackMember); 
+            for (int x = 0; x < vNumber; x++) { 
+                disc[x] = -1; 
+                low[x] = -1; 
+                stackMember[x] = false; 
+                SCCs[x] = -1;
+            }
+
+            for (int x = 0; x < vNumber; x++) 
+                if (disc[x] == -1) 
+                    SCCUtil(x, disc, low, st, stackMember, grades, SCCs); 
+            
+            //delete?
         } 
 
 };
@@ -113,6 +127,8 @@ int main(int argc, char* argv[]) {
         scanf("%d %d", &s1, &s2);
         graph.addEdge(s1, s2);
     }
+
+    delete(grades); // loop?
 
     exit(EXIT_SUCCESS);
 }
